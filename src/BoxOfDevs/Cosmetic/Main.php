9a6f821a0ac7f6058069bc875282b2c3a6321d14
FreeGamingHere <freegaminghere@gmail.com>
2018-06-06 21:15:50 +0300

    Poggit, this is for you...

--- src/BoxOfDevs/Cosmetic/Main.php
+++ src/BoxOfDevs/Cosmetic/Main.php
@@ -496,30 +496,3 @@ class Main extends PluginBase implements Listener {
         $p->setBlockBreaking(false);
     }
 }
-
-class Particles extends PluginTask {
-
-    public function __construct(Main $plugin) {
-        parent::__construct($plugin);
-        $this->plugin = $plugin;
-    }
-    public function onRun($tick) {
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            $name = $player->getName();
-            $level = $player->getLevel();
-            if (in_array($name, $this->plugin->water)) {
-                $particle = new \pocketmine\level\particle\WaterParticle(new Vector3($player->x, $player->y + 2.5, $player->z), 5);
-                $level->addParticle($particle);
-            } elseif (in_array($name, $this->plugin->fire)) {
-                $particle = new \pocketmine\level\particle\EntityFlameParticle(new Vector3($player->x, $player->y + 2.5, $player->z));
-                $level->addParticle($particle);
-            } elseif (in_array($name, $this->plugin->heart)) {
-                $particle = new \pocketmine\level\particle\HeartParticle(new Vector3($player->x, $player->y + 2.5, $player->z), 5);
-                $level->addParticle($particle);
-            } elseif (in_array($name, $this->plugin->smoke)) {
-                $particle = new HugeExplodeParticle(new Vector3($player->x, $player->y + 2.5, $player->z));
-                $level->addParticle($particle);
-            }
-        }
-    }
-}
